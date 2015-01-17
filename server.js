230543a828d3cdf76bb60b4d0c78964868a967b3
Patrick Heneise <patrickheneise@fastmail.com>
2015-01-17 17:23:49 +0100

    removed example json

--- server.js
+++ server.js
@@ -7,43 +7,22 @@ var
 webhook = require('gitup-webhook')(config);
 // twitter = require('./integrations/gitup-twitter')(config);
 
-
-
-/* delivery, example:
-{
-'action': 'labeled',
-'issue': {
-'url': 'https://api.github.com/repos/octocat/Hello-World/issues/1347',
-'number': 1347,
-...
-},
-'repository' : {
-'id': 1296269,
-'full_name': 'octocat/Hello-World',
-'owner': {
-'login': 'octocat',
-'id': 1,
-...
-},
-...
-},
-'sender': {
-'login': 'octocat',
-'id': 1,
-...
-}
-}
-*/
-
 var server = http.createServer(function (request, response) {
   console.log('incoming');
   console.log(request);
 
   webhook.process(request, function (error, event) {
-    response.writeHeader(200, {
-      'Content-Type': 'text/plain'
-    });
-    response.write('OK');
+    if (error) {
+      response.writeHeader(500, {
+        'Content-Type': 'text/plain'
+      });
+      response.write(error);
+    } else {
+      response.writeHeader(500, {
+        'Content-Type': 'application/json'
+      });
+      response.write(event);
+    }
     response.end();
   });
 });
