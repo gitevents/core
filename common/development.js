e31b4e7fed4a57ea882f15d609735b32db6a7645
Josep Boada <josep@h02e56.com>
2015-01-17 18:31:48 +0100

    added config for twitter

--- common/development.js
+++ common/development.js
@@ -5,5 +5,11 @@ module.exports = {
 	},
 	github: {
 		secret: 'Yiet1few0o7Shrac3oV4ghaT5vAit3ak4id5yiF5beaf6yat3B'
+	},
+	twitter:{
+		consumer_key: '',
+	    consumer_secret: '',
+	    token: '',
+	    token_secret: ''
 	}
 };
