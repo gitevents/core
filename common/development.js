e20e548587c4870e2e759c32d8031646fc42ae2d
Patrick Heneise <patrick@fastmail.im>
2015-08-02 15:33:13 +0200

    added config variables

--- common/development.js
+++ common/development.js
@@ -3,6 +3,10 @@ module.exports = {
 	meetup: {
 		apikey: ''
 	},
+	labels: {
+		job: 'jobs',
+		talk: 'talk proposal'
+	},
 	github: {
 		secret: 'Yiet1few0o7Shrac3oV4ghaT5vAit3ak4id5yiF5beaf6yat3B'
 	},
