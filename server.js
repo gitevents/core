7065e5dd5b1c54c9e94ce6263b23d5bb170d5fcc
Patrick Heneise <patrick@fastmail.im>
2015-08-02 15:33:34 +0200

    added GitHub hook verification and handling

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var
-  debug = require('debug')('gitup'),
+  debug = require('debug')('gitevents'),
   config = require('./common/config'),
+  crypto = require('crypto'),
   http = require('http');
 
 // integrations
-webhook = require('gitevents-webhook')(config);
+var webhook = require('gitevents-webhook')(config);
 // twitter = require('./integrations/gitup-twitter')(config);
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
   console.log(req.method + ' ' + req.url);
 
-  process.on('uncaughtException', function (error) {
+  process.on('uncaughtException', function(error) {
     res.writeHeader(500, {
       'Content-Type': 'text/plain'
     });
@@ -22,30 +23,70 @@ var server = http.createServer(function (req, res) {
   if (req.url === '/github/delivery' && req.method === 'POST') {
     var body = '';
 
-    req.on('data', function (chunk) {
+    req.on('data', function(chunk) {
       body += chunk.toString();
     });
 
-    req.on('end', function () {
-      req.body = body;
+    req.on('end', function() {
+      var
+        hmac,
+        calculatedSignature;
 
-      // testing github webhooks.
-      console.log(req);
+      hmac = crypto.createHmac('sha1', config.github.secret);
+      hmac.update(body);
+      calculatedSignature = 'sha1=' + hmac.digest('hex');
 
-      webhook.process(req, function (error, event) {
-        if (error) {
+      // check if payload is coming from GitHub
+      if (req.headers['x-hub-signature'] !== calculatedSignature) {
+        res.writeHeader(403, {
+          'Content-Type': 'text/plain'
+        });
+        res.end();
+      } else {
+        req.body = body;
+        var data;
+
+        try {
+          data = JSON.parse(body);
+        } catch (error) {
+          debug('JSON parse error: ', error);
           res.writeHeader(500, {
             'Content-Type': 'text/plain'
           });
           res.write(error);
-        } else {
-          res.writeHeader(500, {
-            'Content-Type': 'application/json'
-          });
-          res.write(event);
         }
-        res.end();
-      });
+
+        // webhook setup procedure
+        if (data.hook && data.hook.active === true) {
+          res.writeHeader(200, {
+            'Content-Type': 'text/plain'
+          });
+          res.write('Hello GitHub.')
+          res.end();
+        } else {
+          debug('Processing from: ', data.sender.login);
+          webhook.process(data, function(error, event) {
+            if (error) {
+              res.writeHeader(500, {
+                'Content-Type': 'text/plain'
+              });
+              res.write(error);
+            } else {
+              if (!event) {
+                res.writeHeader(204, {
+                  'Content-Type': 'application/json'
+                });
+              } else {
+                res.writeHeader(200, {
+                  'Content-Type': 'application/json'
+                });
+                res.write(event);
+              }
+            }
+            res.end();
+          });
+        }
+      }
     });
   } else {
     res.writeHeader(204, {
@@ -57,4 +98,4 @@ var server = http.createServer(function (req, res) {
 
 server.listen(3000);
 
-console.log('GitUp listening on 3000');
+console.log('GitEvents listening on 3000');
