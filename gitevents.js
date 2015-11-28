a1d2de7f258c467e6bca71bdd6560e1a6c12eb9f
Patrick Heneise <patrick@fastmail.im>
2015-11-28 21:12:01 +0100

    first round of refactoring

--- gitevents.js
+++ gitevents.js
@@ -1,31 +1,85 @@
 var http = require('http');
-var config = require('./common/config');
+var Config = require('./common/config');
 var debug = require('debug')('gitevents');
-var handler = require('./lib/handler');
+var talks = require('./lib/talks');
 var gitWebhook = require('github-webhook-handler');
+var GitHubApi = require('github');
 
-var hookHandler = gitWebhook({
-  path: '/github/delivery',
-  secret: config.github.secret
-});
-
-hookHandler.on('error', function(err) {
-  console.log(err);
-});
-
-hookHandler.on('issues', function(event) {
-  debug('New event: ' + event.event);
-
-  if (event.payload) {
-    handler(event.payload).then(function(onFulfilled, onRejected) {
-      console.log('Done');
-    });
-  }
-});
-
-http.createServer(function(req, res) {
-  hookHandler(req, res, function(err) {
-    res.statusCode = 404;
-    res.end();
+Config.load(function(config) {
+  var hookHandler = gitWebhook({
+    path: '/github/delivery',
+    secret: config.github.secret
   });
-}).listen(3000);
+
+  hookHandler.on('error', function(err) {
+    console.log(err);
+  });
+
+  hookHandler.on('issues', function(event) {
+    debug('New event: ' + event.event);
+
+    if (event.payload) {
+      var payload = event.payload;
+
+      if (payload.action === 'opened') {
+        // nothing to do yet
+      }
+
+      if (payload.action === 'labeled') {
+        var github = new GitHubApi({
+          version: '3.0.0',
+          debug: config.debug,
+          protocol: 'https',
+          timeout: 5000,
+          headers: {
+            'user-agent': 'GitEvents'
+          }
+        });
+
+        github.authenticate({
+          type: 'oauth',
+          token: config.github.token
+        });
+
+        debug('label: ' + payload.label.name);
+
+        var labels = payload.labels.map(function(label) {
+          return label.name;
+        });
+
+        github.user.getFrom({
+          user: payload.sender.login
+        }, function(error, user) {
+          if (error) {
+            console.log('Something went terribly wrong and we don\'t have error reporting set up!');
+            console.log(error);
+            //TODO: add raygun.io / sentry / firebase error reporting here.
+          } else {
+            payload.gitHubUser = user;
+
+            if (labels.indexOf(config.labels.proposal) >= -1) {
+              debug('New talk proposal. Nothing to do yet');
+            }
+
+            if (labels.indexOf(config.labels.talk) >= -1) {
+              debug('New talk: ' + payload.title);
+
+              talks(payload, function(onFulfilled, onRejected) {
+                console.log('talk processed.');
+              });
+            }
+          }
+        });
+      }
+    } else {
+      debug('no action recognised.', event);
+    }
+  });
+
+  http.createServer(function(req, res) {
+    hookHandler(req, res, function(err) {
+      res.statusCode = 404;
+      res.end();
+    });
+  }).listen(3000);
+});
