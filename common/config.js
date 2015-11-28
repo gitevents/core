4ea0415e6518b446ba9422737f0eba96e9fa1c66
Patrick Heneise <patrick@fastmail.im>
2015-11-28 21:11:23 +0100

    new async config loader

--- common/config.js
+++ common/config.js
@@ -1,11 +1,36 @@
-var config;
+var config = {};
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  config = require('./production');
-} else if (process.env.NODE_ENV === 'test') {
-  config = require('./test');
-} else {
-  config = require('./development');
-}
+config.load = function(fn) {
+  if (process.env.NODE_ENV === 'production') {
+    fs.access(path.join(__dirname, 'production.js'), fs.R_OK, function(error, exists) {
+      if (exists) {
+        config = require('./production');
+      } else {
+        var file = fs.createWriteStream(path.join(__dirname, 'production.js'));
+        var url = process.env.CONFIG_URL;
+
+        if (!url) {
+          process.exit(-1);
+        } else {
+          https.get(url, function(response) {
+            response.pipe(file);
+            file.on('finish', function() {
+              file.close(function() {
+                return fn(require('./production'));
+              });
+            });
+          });
+        }
+      }
+    });
+  } else if (process.env.NODE_ENV === 'test') {
+    return fn(require('./test'));
+  } else {
+    return fn(require('./development'));
+  }
+};
 
 module.exports = exports = config;
