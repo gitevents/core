02a0cca30408108105359bf9961b9569c1adea31
Patrick Heneise <patrick@fastmail.im>
2015-11-30 17:05:59 +0100

    test stub

---
+++ tests/talks.js
@@ -0,0 +1,6 @@
+var talks = require('../lib/talks');
+var test = require('tape');
+
+test('issue: opened does nothing.', function(t) {});
+test('issue: labeled "proposal" does nothing.', function(t) {});
+test('issue: labeled "talk" - throw error when a milestone is missing', function(t) {});
